d69742863f51aad924ca71c51e1e45f6158792e3
Carlos Torchia <ctorchia87@gmail.com>
2016-01-10 20:59:20 -0800

    Forgot to credit close icon from google

--- about.js
+++ about.js
@@ -4,5 +4,6 @@ var sourceCodeUrl = "https://github.com/cetorchia/transform-app";
 var libs = [
     "Back arrow icon by Google from www.flaticon.com",
     "Settings cog icon by Google from www.flaticon.com",
+    "Close icon by Google from www.flaticon.com",
     "Qt 5.5 by The Qt Company from www.qt.io"
 ];
